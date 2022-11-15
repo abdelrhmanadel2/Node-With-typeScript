5b43fd33045d4bbdd034202e56e00b6a96d4a807
abdelrhmanadel20 <abdulrahman150274@fci.bu.edu.eg>
2022-11-15 14:19:55 +0200

    adding and configure Prettier + Es-lint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prerrier": 2, //mean error
+    "no-console": 1, //means warning
+    "no-var": "error",
+    "prefer-const": "error",
+  },
+};
