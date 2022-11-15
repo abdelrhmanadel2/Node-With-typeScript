873a912a8a5ef4c923af3d4d2f2c62ab006d4136
abdelrhmanadel20 <abdulrahman150274@fci.bu.edu.eg>
2022-11-15 17:06:52 +0200

    add middle-wares for logging and security
    add middle-wares for logging , security ,rateLimit and requests body parse

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,21 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "eslint-config-prettier",
+  ],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint", "eslint-plugin-prettier"],
   rules: {
-    "prettier/prerrier": 2, //mean error
+    quotes: ["error", "singles"],
+    "prettier/prettier": 2, //mean error
     "no-console": 1, //means warning
     "no-var": "error",
     "prefer-const": "error",
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log("Hello Server")
+import express, { Application } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+const app: Application = express();
+
+// Middlewares
+// http logger
+app.use(morgan("common"));
+//secutity
+app.use(helmet());
+// json parser
+app.use(express.json());
+// apply the Rate Limiting middleware to spesific requests
+// use this for auth
+app.use(
+  "/auth",
+  rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: "Too many requests , please try agian after 5 minutes",
+  })
+);
+// console.log("Hello Server")
+app.listen(3000, () => {
+  console.log("Server listen to port 3000");
+});
